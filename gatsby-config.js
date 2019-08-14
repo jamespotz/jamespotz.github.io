8bfd468192842cb5066c9fc85a0173636a12078b
James Robert Rooke <thepoltergeist23@gmail.com>
2019-08-14 12:17:41 +0800

    update 404 page

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,12 @@ module.exports = {
       options: {
         plugins: [
           {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+          {
             resolve: `gatsby-remark-prismjs`,
             options: {
               classPrefix: "language-",
--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,23 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
+    <h1
+      className="text-6xl text-gray-700 font-display font-bold leading-normal"
+      style={{
+        transform: "perspective(600px) skew(-30deg)",
+      }}
+    >
+      404
+    </h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Link to="/" className="btn mt-2 self-start">
+      Return Home
+    </Link>
   </Layout>
 )
 
