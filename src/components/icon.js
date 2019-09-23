08bee58f17e5b8208d52a6c9c68766593a24a79a
James Robert Rooke <thepoltergeist23@gmail.com>
2019-09-23 10:37:42 +0800

    Updates

--- src/components/icon.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-
-const Icon = ({ name, ...rests }) => <FontAwesomeIcon icon={name} {...rests} />
-
-export default Icon
