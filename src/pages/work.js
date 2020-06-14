1955d0562d6f0684c6e8e194cebff8f4af707689
James Robert Rooke <thepoltergeist23@gmail.com>
2020-06-14 21:17:20 +0800

    update work

--- src/pages/work.js
+++ src/pages/work.js
@@ -33,22 +33,29 @@ const WorkPage = () => {
                 <p>
                   Working on a public Inventory App built with NuxtJs,
                   ExpressJS, NodeJs, MySQL, GraphQL, and PrismaJS. Worked on
-                  Shopify, Woocommerce and Shopify Billing Integration. Worked
-                  on CI/CD deployment using Gitlab CI/CD and Google App Engine.
-                  Worked on the migration from Prisma V1 to Prisma V2. Handles
-                  encryption and the decryption of sensitive keys using Google
-                  Cloud Key Management Service.
+                  Shopify, Woocommerce and Shopify Billing Integration.
                 </p>
-                <p className="mt-2">
-                  Worked on an external project that pulls raw data from Apparel
-                  Magic that can be piped to any Singer IO targets. Used Python3
-                  and Google Cloud Compute Engine.
-                </p>
-                <p className="mt-2">
-                  Worked on an external project for creating an invoice for
-                  Apparel Magic when Shopify fulfills an order. Used Webhook,
-                  Google Cloud Functions, ExpressJS, NodeJs and Axios.
-                </p>
+                <p className="mt-2">Worked on the following:</p>
+                <ul className="list-disc ml-4">
+                  <li>Migration from Prisma V1 to Prisma V2.</li>
+                  <li>
+                    CI/CD deployment using Gitlab CI/CD and Google App Engine.
+                  </li>
+                  <li>
+                    Handles encryption and the decryption of sensitive keys
+                    using Google Cloud Key Management Service.
+                  </li>
+                  <li>
+                    An external project that pulls raw data from Apparel Magic
+                    that can be piped to any Singer IO targets. Used Python3 and
+                    Google Cloud Compute Engine.
+                  </li>
+                  <li>
+                    An external project for creating an invoice for Apparel
+                    Magic when Shopify fulfills an order. Used Webhook, Google
+                    Cloud Functions, ExpressJS, NodeJs, and Axios.
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
@@ -62,18 +69,23 @@ const WorkPage = () => {
             </small>
             <div className="flex flex-col md:flex-row my-4">
               <div className="w-full mt-2 md:mt-0">
-                <p>
-                  Worked on an In-house Document Signing app using ReactJs,
-                  NodeJs, ExpressJS, Axios for ajax request, TailwindCSS,
-                  Material-UI, MomentJS, MongoDB, PassportJs, JWT, Mocha, and
-                  Char for testing.
-                </p>
-                <p className="mt-2">
-                  Worked on Vacation Rental app's frontend using ReactJS and
-                  MomentJS. Worked on separate Vacation Rental app services and
-                  checkout page using ReactJS, Webpack, MomentJS, Bootstrap,
-                  Axios, Redux, and React Router.
-                </p>
+                <p>Worked on the following:</p>
+                <ul className="list-disc ml-4">
+                  <li>
+                    In-house Document Signing app using ReactJs, NodeJs,
+                    ExpressJS, Axios for ajax request, TailwindCSS, Material-UI,
+                    MomentJS, MongoDB, PassportJs, JWT, Mocha, and Char for
+                    testing.
+                  </li>
+                  <li>
+                    Vacation Rental app's frontend using ReactJS and MomentJS.
+                  </li>
+                  <li>
+                    Rental app services and checkout page using ReactJS,
+                    Webpack, MomentJS, Bootstrap, Axios, Redux, and React
+                    Router.
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
@@ -87,14 +99,21 @@ const WorkPage = () => {
             </small>
             <div className="flex flex-col md:flex-row my-4">
               <div className="w-full mt-2 md:mt-0">
-                <p>
-                  Worked on public Ruby on Rails website using Git for the
-                  repository, Heroku for hosting, and Amazon S3 for resource
-                  repository. Created automated email deliveries service with
-                  attached prefilled Compulsory Third Party Liability (CTPL)
-                  forms. Worked on implementing a testing framework using Rspec
-                  and Capybara.
-                </p>
+                <p>Worked on the following:</p>
+                <ul className="list-disc ml-4">
+                  <li>
+                    Public Ruby on Rails website using Git for the repository,
+                    Heroku for hosting, and Amazon S3 for resource repository.
+                  </li>
+                  <li>
+                    Created automated email deliveries service with attached
+                    prefilled Compulsory Third Party Liability (CTPL) forms.
+                  </li>
+                  <li>
+                    Implemented Ruby on Rails testing framework using Rspec and
+                    Capybara.
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
