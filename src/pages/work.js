2d8e26ae2052b16b4d552b170c5e162667e5ffa3
James Robert Rooke <thepoltergeist23@gmail.com>
2020-06-14 16:32:39 +0800

    update work and add wesbos eslint

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,15 +1,15 @@
-import React from "react"
-import { Link } from "gatsby"
-import { useSpring, animated } from "react-spring"
+import React from 'react'
+import { Link } from 'gatsby'
+import { useSpring, animated } from 'react-spring'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { Emoji } from "../components/emoji"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import { Emoji } from '../components/emoji'
 
 const WorkPage = () => {
   const props = useSpring({
-    to: { opacity: 1, transform: "translateX(0)" },
-    from: { opacity: 0.1, transform: "translateX(-100%)" },
+    to: { opacity: 1, transform: 'translateX(0)' },
+    from: { opacity: 0.1, transform: 'translateX(-100%)' },
   })
 
   return (
@@ -22,183 +22,78 @@ const WorkPage = () => {
         <article className="font-default text-gray-700 w-full lg:w-3/4 xl:w-4/12 md:my-10">
           <div>
             <h1 className="font-display leading-normal text-xl font-bold">
-              Soko Labs (2019 - Present)
+              Soko Labs <br />
+              (2019 - Present)
             </h1>
             <small className="text-gray-600 leading-normal ">
               <Emoji symbol="👨‍💻" label="man technology" /> NodeJs Developer
             </small>
+            <div className="flex flex-col md:flex-row my-4">
+              <div className="w-full mt-2 md:mt-0">
+                <p>
+                  Working on a public Inventory App built with NuxtJs,
+                  ExpressJS, NodeJs, MySQL, GraphQL, and PrismaJS. Worked on
+                  Shopify, Woocommerce and Shopify Billing Integration. Worked
+                  on CI/CD deployment using Gitlab CI/CD and Google App Engine.
+                  Worked on the migration from Prisma V1 to Prisma V2. Handles
+                  encryption and the decryption of sensitive keys using Google
+                  Cloud Key Management Service.
+                </p>
+                <p className="mt-2">
+                  Worked on an external project that pulls raw data from Apparel
+                  Magic that can be piped to any Singer IO targets. Used Python3
+                  and Google Cloud Compute Engine.
+                </p>
+                <p className="mt-2">
+                  Worked on an external project for creating an invoice for
+                  Apparel Magic when Shopify fulfills an order. Used Webhook,
+                  Google Cloud Functions, ExpressJS, NodeJs and Axios.
+                </p>
+              </div>
+            </div>
           </div>
           <div className="mt-6">
             <h1 className="font-display leading-normal text-xl font-bold">
-              Maddington Consulting Inc. (2017 - 2019)
+              Maddington Consulting Inc. <br />
+              (2017 - 2019)
             </h1>
             <small className="text-gray-600 leading-normal ">
               <Emoji symbol="👨‍💻" label="man technology" /> Web Developer
             </small>
             <div className="flex flex-col md:flex-row my-4">
-              <div className="w-full md:w-4/12 pr-3">
-                <h1 className="font-bold font-default leading-normal">
-                  <Emoji
-                    symbol="🌐"
-                    label="globe with meridians"
-                    className="mr-2"
-                  />
-                  E-Signature App (in-house)
-                </h1>
-              </div>
-              <div className="w-full mt-2 md:mt-0 md:w-8/12">
+              <div className="w-full mt-2 md:mt-0">
                 <p>
-                  An app to sign and send documents and request signature from
-                  someone and collect it. Primary role as a Front-end and
-                  Backend Developer using
-                  <Emoji symbol="⚛" label="atom" className="mx-1" />
-                  ReactJs,
-                  <Emoji symbol="🕐" label="one o'clock" className="mx-1" />
-                  MomentJs, Axios,
-                  <Emoji
-                    symbol="🍃"
-                    label="leaf fluttering in the wind"
-                    className="mx-1"
-                  />
-                  TailwindCSS, Material-UI,
-                  <Emoji
-                    symbol="◆"
-                    label="large blue diamond"
-                    className="mx-1"
-                  />
-                  NodeJs, ExpressJS, MongoDB, PassportJs, JWT,
-                  <Emoji symbol="☕" label="hot beverage" className="mx-1" />
-                  Mocha and Chai.
+                  Worked on an In-house Document Signing app using ReactJs,
+                  NodeJs, ExpressJS, Axios for ajax request, TailwindCSS,
+                  Material-UI, MomentJS, MongoDB, PassportJs, JWT, Mocha, and
+                  Char for testing.
                 </p>
-              </div>
-            </div>
-            <div className="flex flex-col md:flex-row mb-4">
-              <div className="w-full md:w-4/12 pr-3">
-                <h1 className="font-bold font-default leading-normal">
-                  <Emoji
-                    symbol="🌐"
-                    label="globe with meridians"
-                    className="mr-2"
-                  />
-                  Seaside Summer Rentals
-                </h1>
-              </div>
-              <div className="w-full mt-2 md:mt-0 md:w-8/12">
-                <p>
-                  Mid Cape Cod Vacation Rentals in Dennis and Yarmouth. Primary
-                  role as a Front-end Developer using
-                  <Emoji symbol="⚛" label="atom" className="mx-1" />
-                  ReactJs,
-                  <Emoji symbol="🕐" label="one o'clock" className="mx-1" />
-                  MomentJs,
-                  <Emoji symbol="📦" label="package" className="mx-1" />
-                  Webpack
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col md:flex-row mb-4">
-              <div className="w-full md:w-4/12 pr-3">
-                <h1 className="font-bold font-default leading-normal">
-                  <Emoji
-                    symbol="🌐"
-                    label="globe with meridians"
-                    className="mr-2"
-                  />
-                  Vaycay365
-                </h1>
-              </div>
-              <div className="w-full mt-2 md:mt-0 md:w-8/12">
-                <p>
-                  Cape Cod vacation homes with hotel luxuries. Primary role as a
-                  Front-end Developer using
-                  <Emoji symbol="⚛" label="atom" className="mx-1" />
-                  ReactJs,
-                  <Emoji symbol="🕐" label="one o'clock" className="mx-1" />
-                  MomentJs,
-                  <Emoji symbol="📦" label="package" className="mx-1" />
-                  Webpack
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col md:flex-row mb-4">
-              <div className="w-full md:w-4/12 pr-3">
-                <h1 className="font-bold font-default leading-normal">
-                  <Emoji
-                    symbol="🌐"
-                    label="globe with meridians"
-                    className="mr-2"
-                  />
-                  Vaycay365 services
-                </h1>
-              </div>
-              <div className="w-full mt-2 md:mt-0 md:w-8/12">
-                <p>
-                  Vaycay365 services page and checkout. Primary role as a
-                  Front-end Developer using
-                  <Emoji symbol="⚛" label="atom" className="mx-1" />
-                  ReactJs,
-                  <Emoji symbol="🕐" label="one o'clock" className="mx-1" />
-                  MomentJs, Axios, Material-Ui, Bootstrap, Redux, and React
-                  Router.
+                <p className="mt-2">
+                  Worked on Vacation Rental app's frontend using ReactJS and
+                  MomentJS. Worked on separate Vacation Rental app services and
+                  checkout page using ReactJS, Webpack, MomentJS, Bootstrap,
+                  Axios, Redux, and React Router.
                 </p>
               </div>
             </div>
           </div>
           <div className="mt-6">
             <h1 className="font-display leading-normal text-xl font-bold">
-              Paramount Life & General Insurance Corp. (2014 - 2017)
+              Paramount Life & General Insurance Corp. <br />
+              (2014 - 2017)
             </h1>
             <small className="text-gray-600 leading-normal ">
               <Emoji symbol="👨‍💻" label="man technology" /> Software Engineer
             </small>
             <div className="flex flex-col md:flex-row my-4">
-              <div className="w-full md:w-4/12 pr-3">
-                <h1 className="font-bold font-default leading-normal">
-                  <Emoji
-                    symbol="🌐"
-                    label="globe with meridians"
-                    className="mr-2"
-                  />
-                  ParamountDirect
-                </h1>
-              </div>
-              <div className="w-full mt-2 md:mt-0 md:w-8/12">
+              <div className="w-full mt-2 md:mt-0">
                 <p>
-                  A direct marketing arm of Paramount Life & General Insurance
-                  Corporation. Built with
-                  <Emoji symbol="💎" label="gem stone" className="mx-1" /> Ruby
-                  on Rails, jQuery,
-                  <Emoji symbol="🐘" label="elephant" className="mx-1" />
-                  PostgreSQL, Redis, Bootstrap 3,
-                  <Emoji symbol="📃" label="page with curl" className="mx-1" />
-                  HTML and
-                  <Emoji symbol="🛡" label="shield" className="mx-1" />
-                  CSS.
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col md:flex-row mb-4">
-              <div className="w-full md:w-4/12 pr-3">
-                <h1 className="font-bold font-default leading-normal">
-                  <Emoji
-                    symbol="🌐"
-                    label="globe with meridians"
-                    className="mr-2"
-                  />
-                  CTPL
-                </h1>
-              </div>
-              <div className="w-full mt-2 md:mt-0 md:w-8/12">
-                <p>
-                  CTPL is a car insurance application page. Built with
-                  <Emoji symbol="💎" label="gem stone" className="mx-1" /> Ruby
-                  on Rails, jQuery,
-                  <Emoji symbol="🐘" label="elephant" className="mx-1" />
-                  PostgreSQL, Redis, Bootstrap 3,
-                  <Emoji symbol="📃" label="page with curl" className="mx-1" />
-                  HTML and
-                  <Emoji symbol="🛡" label="shield" className="mx-1" />
-                  CSS.
+                  Worked on public Ruby on Rails website using Git for the
+                  repository, Heroku for hosting, and Amazon S3 for resource
+                  repository. Created automated email deliveries service with
+                  attached prefilled Compulsory Third Party Liability (CTPL)
+                  forms. Worked on implementing a testing framework using Rspec
+                  and Capybara.
                 </p>
               </div>
             </div>
