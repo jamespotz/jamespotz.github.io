b8cb0081c16964dbb1b35651e57bd425377cc9e8
James Robert Rooke <thepoltergeist23@gmail.com>
2020-09-19 15:29:19 +0800

    Update avatar and work

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import { useSpring, animated } from 'react-spring'
+import React from "react"
+import { Link } from "gatsby"
+import { useSpring, animated } from "react-spring"
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
-import { Emoji } from '../components/emoji'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { Emoji } from "../components/emoji"
 
 const WorkPage = () => {
   const props = useSpring({
-    to: { opacity: 1, transform: 'translateX(0)' },
-    from: { opacity: 0.1, transform: 'translateX(-100%)' },
+    to: { opacity: 1, transform: "translateX(0)" },
+    from: { opacity: 0.1, transform: "translateX(-100%)" },
   })
 
   return (
@@ -20,24 +20,26 @@ const WorkPage = () => {
       </Link>
       <animated.div style={props} className="flex flex-col">
         <article className="font-default text-gray-700 w-full lg:w-3/4 xl:w-4/12 md:my-10">
-          <div>
+          <div className="mt-6">
             <h1 className="font-display leading-normal text-xl font-bold">
-              Dev Partners <br />
-              (2019 - Present)
+              TechStyle Fashion Group
+              <br />
+              (2020 - Present)
             </h1>
             <small className="text-gray-600 leading-normal ">
               <Emoji symbol="👨‍💻" label="man technology" /> Software Developer
             </small>
-            <div className="flex flex-col md:flex-row my-4">
-              <div className="w-full mt-2 md:mt-0">
-                <p>
-                  Working at TechStyle Fashion Group as a Rapid Response
-                  Developer
-                </p>
-              </div>
-            </div>
           </div>
-          <div>
+          <div className="mt-6">
+            <h1 className="font-display leading-normal text-xl font-bold">
+              Dev Partners <br />
+              (2020 - Present)
+            </h1>
+            <small className="text-gray-600 leading-normal ">
+              <Emoji symbol="👨‍💻" label="man technology" /> Software Developer
+            </small>
+          </div>
+          <div className="mt-6">
             <h1 className="font-display leading-normal text-xl font-bold">
               Soko Labs <br />
               (2019 - 2020)
